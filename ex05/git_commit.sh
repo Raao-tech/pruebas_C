f80bb9ac8fd725d5796e0323cdd400eda5a437be
Rafael Araujo Ordono <rafarauj@c2r8s3.42madrid.com>
2025-07-07 15:52:21 +0200

    added file git_commit.sh modify

--- ex05/git_commit.sh
+++ ex05/git_commit.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Define el número de commits a mostrar
-NUM_COMMITS=5
+
 
 # Ejecuta el comando git log para obtener los últimos commits
-git log -n $NUM_COMMITS --oneline
+git log  -n 5
